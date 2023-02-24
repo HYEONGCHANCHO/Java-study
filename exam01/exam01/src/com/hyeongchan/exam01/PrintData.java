a9a637bf31dbb8b14733d32d2bb9f5b7c6681e87
CHO HYEONGCHAN <118033064+HYEONGCHANCHO@users.noreply.github.com>
2023-02-24 19:56:40 +0900

    Java-study

---
+++ exam01/exam01/src/com/hyeongchan/exam01/PrintData.java
@@ -0,0 +1,8 @@
+package com.hyeongchan.exam01;
+// 주석
+public class PrintData { //worker class
+	String iname="조형찬";
+	public void consolePrint() { //worker method
+		System.out.println("Hello World");
+	}
+}
